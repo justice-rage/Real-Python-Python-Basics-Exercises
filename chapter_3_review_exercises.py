a4f6b82f04764a850826b88cf5adda116554bc10
Justice Peltier <peltierjustice@gmail.com>
2020-08-29 19:36:42 -0400

    chapter_3_review_exercises
    Chapter 3: Your First Python Program
    
    Three review exercises on displaying text with the print function, as well as assigning string literals to variables.

---
+++ chapter_3_review_exercises.py
@@ -0,0 +1,19 @@
+# Chapter 3 Review Exercises
+# Section 3.3: Create a Variable
+
+# 1) Using the interactive window display some text using print() .
+
+print("Hello world!")
+
+# 2) Using the interactive window, assign a string literal to a variable.
+# Then print the contents of the variable using the print statement.
+
+var_1 = "This is a string literal."
+print(var_1)
+
+# 3) Repeat the first two excercises using the editor window.
+# Note to reader: Imagine this was done in an editor.
+
+print("Some text.")
+var_2 = "This is another string literal!"
+print(var_2)
