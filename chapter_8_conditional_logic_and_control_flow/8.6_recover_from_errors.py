b9cb4d1ad10e59cfd7b3b92fdd159fc147a2dfcf
Justice Peltier <peltierjustice@gmail.com>
2020-09-05 19:02:07 -0400

    Initial commit, incomplete

---
+++ chapter_8_conditional_logic_and_control_flow/8.6_recover_from_errors.py
@@ -0,0 +1,13 @@
+# 8.6 Recover From Errors
+
+# 1) Write a program that repeatedly asks the user to input an integer, 
+# displaying a message to “try again” by catching the ValueError that 
+# is raised if the user did not enter an integer.
+# Once the user enters an integer, the program should display the number
+#  back to the user and end without crashing.
+
+# 2) Write a program that asks the user to input a string and an integer n. 
+# Then display the character at index n in the string.
+# Use error handling to make sure the program doesn’t crash if the user does
+# not enter an integer or the index is out of bounds. The program should 
+# display a different message depending on what error occurs.
