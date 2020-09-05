7f9b9af0fa0fc24e6a9fbbff3f45816d306b9701
Justice Peltier <peltierjustice@gmail.com>
2020-09-05 11:56:56 -0400

    Initial push

---
+++ chapter_8_conditional_logic_and_control_flow/8.1_compare_values.py
@@ -0,0 +1,21 @@
+# 8.1 Compare Values
+
+# 1) For each of the following conditional expressions, guess whether they 
+# evaluate to True or False . Then type them into the interactive window to
+# check your answers:
+
+1 <= 1 # Guess: True
+1 != 1 # Guess: False
+1 != 2 # Guess: True
+"good" != "bad" # Guess: True
+"good" != "Good" # Guess: True
+123 == "123" # Guess: False
+
+# 2) For each of the following expressions, fill in the blank 
+# (indicated by __ ) with an appropriate boolean comparator so 
+# that the expression evaluates to True :
+
+3 __ 4 # 3 < 4
+10 __ 5 # 10 > 5
+"jack" __ "jill" # "jack" != "jill"
+42 __ "42" # 42 != "42"
