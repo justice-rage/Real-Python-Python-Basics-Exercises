9922307858860b82a76b15803286a51d2899bd1d
Justice Peltier <peltierjustice@gmail.com>
2020-09-12 19:12:55 -0400

    Correction, import random

--- chapter_8_conditional_logic_and_control_flow/8.7_simulate_events_and_calculate_probabilities.py
+++ chapter_8_conditional_logic_and_control_flow/8.7_simulate_events_and_calculate_probabilities.py
@@ -1,6 +1,6 @@
 # 8.7 Simulate Events and Calculate Probabilities
 
-import random
+from random import randint
 
 # 1) Write a function called roll() that uses the randint() function to 
 # simulate rolling a fair die by returning a random integer between 1 and 6 .
@@ -11,3 +11,5 @@ def roll():
 
 # 2) Write a program that simulates 10,000 rolls of a fair die and displays 
 # the average number rolled.
+
+print(roll())
