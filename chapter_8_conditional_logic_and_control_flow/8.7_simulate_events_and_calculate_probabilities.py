06abd8df3cadd013773609e478ff9f90b8320c41
Justice Peltier <peltierjustice@gmail.com>
2020-09-12 20:42:17 -0400

    Exercise #2 solved
    Utilized statistics library to import mean.

--- chapter_8_conditional_logic_and_control_flow/8.7_simulate_events_and_calculate_probabilities.py
+++ chapter_8_conditional_logic_and_control_flow/8.7_simulate_events_and_calculate_probabilities.py
@@ -1,6 +1,7 @@
 # 8.7 Simulate Events and Calculate Probabilities
 
 from random import randint
+from statistics import mean
 
 # 1) Write a function called roll() that uses the randint() function to 
 # simulate rolling a fair die by returning a random integer between 1 and 6 .
@@ -12,4 +13,9 @@ def roll():
 # 2) Write a program that simulates 10,000 rolls of a fair die and displays 
 # the average number rolled.
 
-print(roll())
+sum = []
+
+for i in range(10_000):
+    sum.append(roll())
+
+print(f"Average result of 10,000 rolls: {mean(sum)}")
