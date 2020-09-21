995ccbf2c49a33b099d77d6b1bb78d48433ad709
Justice Peltier <peltierjustice@gmail.com>
2020-09-21 19:39:51 -0400

    Create 9.3_nesting_copying_and_sorting_tuples_and_lists.py

---
+++ chapter_9_tuples_lists_and_dictionaries/9.3_nesting_copying_and_sorting_tuples_and_lists.py
@@ -0,0 +1,15 @@
+# 9.3 Nesting, Copying and Sorting Tuples and Lists
+
+# 1) Create a tuple data with two values. The first value should be the 
+# tuple (1, 2) and the second value should be the tuple (3, 4) .
+
+# 2) Write a for loop that loops over data and prints the sum of each nested tuple. 
+# The output should look like this: 
+# Row 1 sum: 3
+# Row 2 sum: 7 
+
+# 3) Create the following list [4, 3, 2, 1] and assign it to the variable numbers .
+
+# 4) Create a copy of the numbers list using the [:] slicing notation.
+
+# 5) Sort the numbers list in numerical order using the .sort() method.
